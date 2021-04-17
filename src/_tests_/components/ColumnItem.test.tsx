28d0817a47f3a1c121ee2ed02fed6ef775f7c46a
stefanos athanasoulias <stefanos.athanasoulas@ncryptedcloud.com>
2021-04-17 15:06:20 +0300

    More unit tests added

--- src/_tests_/components/ColumnItem.test.tsx
+++ src/_tests_/components/ColumnItem.test.tsx
@@ -2,29 +2,62 @@ import { render, RenderResult, configure } from '@testing-library/react'
 import ColumnItem from '../../components/ColumnItem';
 import { mockColumnEntities } from '../mocks/mockColumnEntities';
 import TasksView from "../../containers/TasksView";
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import TaskItem from '../../components/TaskItem';
 
-configure({testIdAttribute: 'id'})
+configure({ testIdAttribute: 'id' })
 
 let documentBody: RenderResult;
 
 describe("<TasksView />", () => {
-  beforeEach(() => {
-    documentBody = render(<TasksView />);
-  });
+    beforeEach(() => {
+        documentBody = render(<TasksView />);
+    });
 
-  test("Renders Column component correctly",  async () => {
+    test("Renders Column component correctly", async () => {
 
-   expect(await documentBody.findByTestId('test-container')).toBeVisible();
+        expect(await documentBody.findByTestId('test-container')).toBeVisible();
         const dom = render(
             <DragDropContext onDragEnd={jest.fn()}>
-                <ColumnItem  
+                <ColumnItem
                     column={mockColumnEntities[132]}
                     handleRemoveColumn={jest.fn()}
                 />
             </DragDropContext>
         );
 
-   expect(await dom.queryAllByText(/Add task/i)).toHaveLength(2);
-  });
+        expect(await dom.queryAllByText(/Add task/i)).toHaveLength(2);
+    });
+
+    test("Renders at least one task", async () => {
+        const task = mockColumnEntities[132].taskList[0];
+        const column = mockColumnEntities[132];
+
+        const dom = render(
+            <DragDropContext onDragEnd={jest.fn()}>
+                <Droppable droppableId={String(column.id)}>
+                    {(provided) => (
+                        <div className="column" {...provided.droppableProps} ref={provided.innerRef}>
+
+                            <Draggable draggableId={String(task.id)} index={1} key={String(task.id)}>
+                                {(provided) => (
+
+                                    <TaskItem
+                                        provided={provided}
+                                        columnId={column.id}
+                                        task={task}
+                                        handleRemoveTask={jest.fn()}
+                                    />
+                                )}
+                            </Draggable>
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        );
+
+        expect( dom.getByText(/Test second description/i)).toBeInTheDocument();
+
+    });
 });
